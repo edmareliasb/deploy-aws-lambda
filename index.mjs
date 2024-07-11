3c9adcc70bb1f3a983357d7f73315b3512ef3c29
edmarelias25 <edmarelias25@yahoo.com.br>
2024-07-11 15:07:37 -0300

    lendo arquivo

--- index.mjs
+++ index.mjs
@@ -1,13 +1,29 @@
+import { s3 } from '@aws-sdk/client-s3';
 import { log } from "./log.mjs";
 
-export const handler = async (event) => {
-    
-    log('Event: ' + JSON.stringify(event));
 
-    const response = {
-      statusCode: 200,
-      body: JSON.stringify(event),
-    };
-    return response;
+const s3Client = new S3({ region: 'us-east-1' })
+
+export const handler = async (event) => {
+
+    const record = event.Records[0];
+    const Bucket = record.s3.bucket.name;
+    const key = record.s3.object.key;
+
+    const getObjectResult = await s3Client.getObject({
+        Bucket,
+        Key,
+    });
+
+    const megaByte = 1024 * 1024;
+
+    if (getObjectResult.ContentLength > megaByte) {
+        log('Objeto muito grande');
+        return 'Objeto muito grande';
+    }
+
+    log('Objeto de tamanho OK');
+
+    return 'Objeto de tamanho OK';
   };
   
